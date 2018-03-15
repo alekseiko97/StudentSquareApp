831fec4340b1b5c2d89d4d9e540769cc4378d233
Moskaljuk, Aleksei A <alekseiko@gmail.com>
2018-03-15 10:27:46 +0100

    Firebase authentication is made

---
+++ StudentSquareApp/app/src/main/java/com/fhict/studentsquareapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.fhict.studentsquareapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                auth.signOut();
+            }
+        });
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    finish();
+                    Toast.makeText(getApplicationContext(), "Logged out!", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        };
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        auth.addAuthStateListener(authListener);
+    }
+}
